2d3e98d220144d5f7e820e2d5e5c960726fabc1c
Oshimani <jannick2.0@googlemail.com>
2023-07-25 01:15:00 +0200

    init

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,10 @@
+// intro2.rs
+// Make the code print a greeting to the world.
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
+
+
+fn main() {
+    let var: &str = "World";
+    let count: f64 = 2.0 / 3.0;
+    println!("Hello {var}! {count} is not a very large number.");
+}
---
+++ exercises/vecs/vecs1.rs
@@ -0,0 +1,38 @@
+// vecs1.rs
+// Your task is to create a `Vec` which holds the exact same elements
+// as in the array `a`.
+// Make me compile and pass the test!
+// Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
+
+
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // a plain array
+
+    // METHOD 1
+    // let v = a.to_vec(); // TODO: declare your vector here with the macro for vectors
+
+    // METHOD 2
+    // let mut v = Vec::new();
+    // v.push(a[0]);
+    // v.push(a[1]);
+    // v.push(a[2]);
+    // v.push(a[3]);
+
+    // METHOD 3
+    let mut v = vec![];
+    for e in a {
+        v.push(e);
+    }
+    (a, v)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, v[..]);
+    }
+}
